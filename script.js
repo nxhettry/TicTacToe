e403e28b91f461f3cdfa767443cc76d0b0e449ec
nxhettry <nishan.gtsn@gmail.com>
2024-02-06 20:27:51 +0545

    Added singleplayer mode to the game.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var title = document.querySelector("h1");
-var boxes = document.querySelectorAll(".box");
-var resetBtn = document.querySelector(".rstBtn");
-var turn = true;
+let title = document.querySelector("h1");
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".rstBtn");
+let turn = true;
 let turnCount = 0;
 const winCase = [
     [0, 1, 2],
@@ -13,7 +13,6 @@ const winCase = [
     [0, 4, 8],
     [2, 4, 6],
 ];
-
 title.addEventListener("mouseover", () => {
     title.classList.add("title");
     title.classList.remove("heading");
@@ -30,6 +29,24 @@ resetBtn.addEventListener("mouseout", () => {
     resetBtn.style.backgroundColor = "";
     resetBtn.style.color = "";
 });
+const checkAns = () => {
+    for (let pattern of winCase) {
+        let firstVal = boxes[pattern[0]].innerText;
+        let secondVal = boxes[pattern[1]].innerText;
+        let thirdVal = boxes[pattern[2]].innerText;
+        if (firstVal != "" && secondVal != "" && thirdVal != "") {
+            if (firstVal === secondVal && secondVal === thirdVal) {
+                title.innerText = `${firstVal} wins ! Congrats 🎉🎉`;
+                resetBtn.innerText = "Restart";
+                title.classList.add("result");
+                title.classList.remove("heading");
+                disableBoxes();
+                return true;
+            }
+        }
+    }
+
+};
 const reset = () => {
     turn = true;
     turnCount = 0;
@@ -37,34 +54,17 @@ const reset = () => {
     title.innerText = "Tic-Tac-Toe";
     title.classList.add("heading");
     title.classList.remove("result");
+    resetBtn.innerText = "Reset";
 };
 resetBtn.addEventListener("click", () => {
     boxes.forEach((box) => {
         reset();
     })
 });
-
-boxes.forEach((box) => {
-    box.addEventListener("click", function () {
-        if (turn) {
-            box.innerText = "O";
-            turn = !turn;
-        } else {
-            box.innerText = "X";
-            turn = true;
-        }
-        box.disabled = true;
-        turnCount++;
-        let winAction = checkAns();
-        if (turnCount === 9 && !winAction) {
-            draw();
-        }
-    });
-});
-
 const draw = () => {
-    title.innerText = "Draw !!! Press the reset button to play again.";
+    title.innerText = "Draw !!! Press the restart button to play again.";
     disableBoxes();
+    resetBtn.innerText = "Restart";
 };
 
 const enableBoxes = () => {
@@ -78,21 +78,66 @@ const disableBoxes = () => {
         box.disabled = true;
     }
 };
-
-const checkAns = () => {
-    for (let pattern of winCase) {
-        let firstVal = boxes[pattern[0]].innerText;
-        let secondVal = boxes[pattern[1]].innerText;
-        let thirdVal = boxes[pattern[2]].innerText;
-        if (firstVal != "" && secondVal != "" && thirdVal != "") {
-            if (firstVal === secondVal && secondVal === thirdVal) {
-                title.innerText = `${firstVal} wins ! Congrats 🎉🎉`;
-                title.classList.add("result");
-                title.classList.remove("heading");
-                disableBoxes();
-                return true;
-            }
-        }
+window.onload = () => {
+    let mode = prompt("Enter 1 for singleplayer OR 2 for multiplayer !");
+    if (mode == 1) {
+        singlePlayer();
     }
+    else if (mode == 2) {
+        multiPlayer();
+    }
+}
+const singlePlayer = () => {
+    boxes.forEach((box) => {
+        box.addEventListener("click", function () {
+            if (turn) {
+                box.innerText = "O";
+                turn = !turn;
+                setTimeout(() => {
+                    botMove();
+                }, 500);
+            }
+            box.disabled = true;
+            turnCount++;
+            let winAction = checkAns();
+            if (turnCount === 9 && !winAction) {
+                draw();
+            }
+        });
+    });
+}
+const botMove = () => {
+    let botChoice;
+    do{
+        botChoice = boxes[Math.floor(Math.random() * 9)];
+    }
+    while(botChoice.innerText != "");
+    botChoice.innerText = "X";
+    turn = true;
+    botChoice.disabled = true;
+    turnCount++;
+    let x = checkAns();
+    if (turnCount === 9 && !x) {
+        draw();
+    }
+}
+const multiPlayer = () => {
 
+    boxes.forEach((box) => {
+        box.addEventListener("click", function () {
+            if (turn) {
+                box.innerText = "O";
+                turn = !turn;
+            } else {
+                box.innerText = "X";
+                turn = true;
+            }
+            box.disabled = true;
+            turnCount++;
+            let winAction = checkAns();
+            if (turnCount === 9 && !winAction) {
+                draw();
+            }
+        });
+    });
 };
